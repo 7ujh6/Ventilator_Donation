f2d11af79d7b9e3295ce6b45a6566a886f904820
stant <stantondobson@gmail.com>
2020-05-16 09:52:03 -0400

    moved build directory up

---
+++ build/precache-manifest.dbfc1cb58c4065af18997586c681d555.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca554b3203ba1a7171bf1b956dd59ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b88b63501e27586df79",
+    "url": "/static/css/main.d2ac2821.chunk.css"
+  },
+  {
+    "revision": "188bcd0ad954a4651714",
+    "url": "/static/js/2.9c1d64cc.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.9c1d64cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b88b63501e27586df79",
+    "url": "/static/js/main.cd76bb1a.chunk.js"
+  },
+  {
+    "revision": "0b03def983958de4eedb",
+    "url": "/static/js/runtime-main.d479f090.js"
+  },
+  {
+    "revision": "269eeef9599e717f7158d35bc86d2f8d",
+    "url": "/static/media/2020-05-09 15_56_50-React App.269eeef9.png"
+  },
+  {
+    "revision": "e27c8735da98ec6ccdcf12e258b26475",
+    "url": "/static/media/default-profile-icon.e27c8735.png"
+  },
+  {
+    "revision": "3df1ec843241345988d13da380c9a839",
+    "url": "/static/media/logo.3df1ec84.svg"
+  },
+  {
+    "revision": "d37d94008347a67f103811601107af7f",
+    "url": "/static/media/pencil-edit-button.d37d9400.svg"
+  },
+  {
+    "revision": "aa922b9a44fd3421002f8beaf817f62e",
+    "url": "/static/media/plus_symbol.aa922b9a.svg"
+  }
+]);
